34f79aa4e0183c1bda46f4fc2662637ba450a1c1
babakfp <babak.bxf@gmail.com>
2024-07-13 01:09:57 +0330

    Enabled container and some config futures

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,4 +4,14 @@ import { allAddons } from "tailwindcss-addons"
 export default {
     content: ["./src/**/*.{html,svelte}"],
     plugins: [...allAddons()],
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+        },
+    },
+    future: {
+        hoverOnlyWhenSupported: true,
+        disableColorOpacityUtilitiesByDefault: true,
+    },
 } satisfies Config
