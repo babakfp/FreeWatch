61e7903c3dcca9ff7f65687a61d4827598f47641
babakfp <babak.bxf@gmail.com>
2024-07-13 01:59:49 +0330

    Added title and description

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,14 @@
     import { platforms } from "$lib/data.js"
 </script>
 
+<svelte:head>
+    <title>FreeWatch</title>
+    <meta
+        name="description"
+        content="List of free alternatives to Netflix, Amazon Prime Video, HBO Max, and Disney+. Download (and Watch Online) Movies, Series and Anime for free."
+    />
+</svelte:head>
+
 <main class="container pb-16 pt-8">
     <ul class="grid gap-4">
         {#each platforms as platform}
