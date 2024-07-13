21f8cdf466d00877e1c911f1e261fa045b90bb8c
babakfp <babak.bxf@gmail.com>
2024-07-13 20:12:17 +0330

    Updated footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -28,13 +28,31 @@
     </ul>
 </main>
 
-<div class="border-t border-gray-800 py-4 text-center">
-    <p class="text-sm">
-        Favicon by
-        <a
-            class="text-accent underline"
-            href="https://icons8.com"
-            target="_blank">Icon8</a
-        >.
-    </p>
-</div>
+<footer class="container border-t border-gray-800 py-8">
+    <ul class="list-inside list-disc space-y-2 text-sm">
+        <li>
+            Made by
+            <a
+                class="text-accent underline"
+                href="https://babakfp.ir"
+                target="_blank">Babak</a
+            >.
+        </li>
+        <li>
+            Source code on
+            <a
+                class="text-accent underline"
+                href="https://github.com/babakfp/FreeWatch"
+                target="_blank">GitHub</a
+            >.
+        </li>
+        <li>
+            Favicon by
+            <a
+                class="text-accent underline"
+                href="https://icons8.com"
+                target="_blank">Icon8</a
+            >.
+        </li>
+    </ul>
+</footer>
