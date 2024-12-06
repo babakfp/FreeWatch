2dc81ee8aead6301e26c20f2b1fb36521f970d85
babakfp <babak.bxf@gmail.com>
2024-12-06 04:25:56 +0330

    Stuff

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,34 @@
+<footer class="border-t border-gray-800 py-8">
+    <ul class="container list-inside list-disc space-y-2 text-sm">
+        <li>
+            Made by
+            <a
+                class="text-accent underline"
+                href="https://babakfp.ir"
+                target="_blank"
+            >
+                Babak
+            </a>
+        </li>
+        <li>
+            Source code on
+            <a
+                class="text-accent underline"
+                href="https://github.com/babakfp/FreeWatch"
+                target="_blank"
+            >
+                GitHub
+            </a>
+        </li>
+        <li>
+            Favicon by
+            <a
+                class="text-accent underline"
+                href="https://icons8.com"
+                target="_blank"
+            >
+                Icon8
+            </a>
+        </li>
+    </ul>
+</footer>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,47 +1,10 @@
 <script lang="ts">
     import "$lib/app.css"
-    import type { Snippet } from "svelte"
+    import Footer from "$lib/components/Footer.svelte"
 
-    let {
-        children,
-    }: {
-        children: Snippet
-    } = $props()
+    let { children } = $props()
 </script>
 
 {@render children()}
 
-<footer class="border-t border-gray-800 py-8">
-    <ul class="container list-inside list-disc space-y-2 text-sm">
-        <li>
-            Made by
-            <a
-                class="text-accent underline"
-                href="https://babakfp.ir"
-                target="_blank"
-            >
-                Babak
-            </a>
-        </li>
-        <li>
-            Source code on
-            <a
-                class="text-accent underline"
-                href="https://github.com/babakfp/FreeWatch"
-                target="_blank"
-            >
-                GitHub
-            </a>
-        </li>
-        <li>
-            Favicon by
-            <a
-                class="text-accent underline"
-                href="https://icons8.com"
-                target="_blank"
-            >
-                Icon8
-            </a>
-        </li>
-    </ul>
-</footer>
+<Footer />
